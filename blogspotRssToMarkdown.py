da121a68983949bbd70c3d1ad73d181fa958ff3c
Robbi Nespu <robbinespu@gmail.com>
2021-07-07 13:15:24 +0800

    fetch them all

--- blogspotRssToMarkdown.py
+++ blogspotRssToMarkdown.py
@@ -23,42 +23,59 @@
 import feedparser
 import os.path
 from markdownify import markdownify
+from urllib.parse import urlparse
 directory = './html/'
 # nenez9595.blogspot.com/feeds/posts/default?start-index=3?max-results=500
 # nenez9595.blogspot.com/feeds/posts/default?n=1000
 # nenez9595.blogspot.com/feeds/posts/default?alt=json
-rss_url = "https://nenez9595.blogspot.com/feeds/posts/default?max-results=500"
+#rss_url = "https://nenez9595.blogspot.com/feeds/posts/default"
 #NewsFeed = feedparser.parse(rss_url)
 #entry = NewsFeed.entries[1]
-#print(entry.keys())
+#print(entry.keys()) 
+# # dict_keys(['id', 'guidislink', 'link', 'published', 'published_parsed', 'updated', 'updated_parsed', 'title', 'title_detail', 'content', 'summary', 'links', 'authors', 'author_detail', 'author', 'gd_image', 'thr_total'])
+#print(entry) 
 
-feed = feedparser.parse( rss_url )
-items = feed["items"]
-for item in items:
-    #print(item)
-    time = item[ "published_parsed" ]
-    title = item[ "title" ]
-    slug = item[ "title" ].replace(' ', '-').lower()
-    year = str(time.tm_year)
-    mon = str(time.tm_mon)
-    day = str(time.tm_mday)
-    clock= item[ "updated" ]
-    #print("Time:"+year + '-' + mon  + '-' + day)
-    #print("Title:"+title)
-    link = item[ "link" ]
-    print("Link - "+link)
-    fileName = year + '-' + mon  + '-' + day + '-' + slug + '.md'
-    fileName = fileName.replace('/', '')
-    file_path = os.path.join(directory, fileName)
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
-    f = open(file_path,'w')
-    value = item["content"][0]['value']
-    f.write('---\nlayout: post\ntitle: "' + title + '"\n')
-    f.write('\ndate: ' + clock + '\n')
-    f.write('draft: false\ntype: post\n')
-    f.write('---\n')
-    f.write(markdownify(value))
-    f.write('\nLink: ['+link+']('+link+')\n')
-    f.close()
-print('end')
+def main():
+    # Opening file
+    file1 = open('ayam', 'r')
+    count = 0
+    # Using for loop
+    print("Using for loop")
+    for line in file1:
+        count += 1
+        print("Scraping rss feed {}- {}".format(count, line.strip()))
+        feed = feedparser.parse(line.strip() )
+        items = feed["items"]
+        for item in items:
+            #print(item)
+            time = item[ "published_parsed" ]
+            title = item[ "title" ]
+            slug = item[ "title" ].replace(' ', '-').lower().replace('?', '').replace('#', '')
+            year = str(time.tm_year)
+            mon = str(time.tm_mon)
+            day = str(time.tm_mday)
+            clock= item[ "updated" ]
+            #print("Time:"+year + '-' + mon  + '-' + day)
+            #print("Title:"+title)
+            link = item[ "link" ]
+            #print("Link - "+link)
+            fileName = year + '-' + mon  + '-' + day + '-' + slug + '.md'
+            fileName = fileName.replace('/', '')
+            file_path = os.path.join(directory, fileName)
+            if not os.path.isdir(directory):
+                os.mkdir(directory)
+            f = open(file_path,'w')
+            value = item["content"][0]['value']
+            f.write('---\nlayout: post\ntitle: "' + title + '"\n')
+            f.write('\ndate: ' + clock + '\n')
+            f.write('draft: false\ntype: post\n')
+            f.write('---\n')
+            f.write(markdownify(value))
+            f.write('\nLink: ['+link+']('+link+')\n')
+            f.close()
+        print('end')
+    # Closing files
+    file1.close()
+
+if __name__ == "__main__":
+    main()
